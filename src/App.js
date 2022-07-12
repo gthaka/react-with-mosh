20396094b7aa1e78b50a340bb31a318524e80e19
Anthony G <agithaka@live.com>
2022-07-12 18:29:30 +0300

    5th Counter - NodeJS Action Test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       { id: 1, value: 4 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
-      { id: 4, value: 0 }
+      { id: 4, value: 0 },
+      { id: 5, value: 0 }
     ]
   };
   constructor() {
@@ -30,10 +31,10 @@ class App extends Component {
     // console.log(counter);
     const counters = [...this.state.counters];
     const index = counters.indexOf(counter);
-    counters[index] = {...counter};
+    counters[index] = { ...counter };
     counters[index].value++;
     this.setState({ counters });
-    
+
     // console.log(this.state.counters[0]);
   }
 
@@ -42,22 +43,22 @@ class App extends Component {
     this.setState({ counters });
   }
 
-  render() { 
+  render() {
     console.log('App - Rendered!');
     return (
-    <React.Fragment>
-        <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length}/>
-      <main className="container" >
+      <React.Fragment>
+        <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length} />
+        <main className="container" >
           <Counters
             counters={this.state.counters}
             onReset={this.handleReset}
             onDelete={this.handleDelete}
             onIncrement={this.handlerIncrement}
           />
-      </main>
-    </React.Fragment>
+        </main>
+      </React.Fragment>
     );
   }
 }
- 
+
 export default App;
